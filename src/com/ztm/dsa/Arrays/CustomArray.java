24f40b578a083235decfecd063f9b6d30a2e1e16
Kajal Tiwari <Kajal.Tiwari348@gmail.com>
2022-05-20 17:27:55 +0530

    Array implementations

--- src/com/ztm/dsa/Arrays/CustomArray.java
+++ src/com/ztm/dsa/Arrays/CustomArray.java
@@ -1,29 +1,30 @@
-//package com.ztm.dsa.Arrays;
-//
-//public class CustomArray {
-//    private int length;
-//    String[] data;
-//
-//    public CustomArray() {
-//        this.length = 0;
-//        this.data = new String[]{};
-//    }
-//
-//    public String getData(int index) {
-//        return data[index];
-//    }
-//    public int setData(String item) {
-//        data[length]=item;
-//        length++;
-//        return length;
-//    }
-//
-//    public static void main(String[] args) {
-//        CustomArray newArray = new CustomArray();
-//        newArray.setData("Rest");
-//        System.out.println(newArray.getData(0));
-//
-//    }
-//
-//}
-//
+package com.ztm.dsa.Arrays;
+
+public class CustomArray {
+    private int length;
+    String[] data;
+
+    public CustomArray(int len, String[] arr) {
+        this.length = 0;
+        this.data = new String[100];
+    }
+
+    public String getData(int index) {
+        return data[index];
+    }
+    public int setData(String item) {
+        this.data[this.length]=item;
+        this.length++;
+        return this.length;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {};
+        CustomArray newArray = new CustomArray(0, arr);
+        newArray.setData("Rest");
+        System.out.println(newArray.getData(0));
+
+    }
+
+}
+
